038285aa7f2b8930cd419d55d11f77d64020e7d0
Drew Fout <drewmf1566@gmail.com>
2024-07-10 15:33:27 -0500

    storeItemImage model

---
+++ app/models/Store/storeItemImage.py
@@ -0,0 +1,23 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class storeItemImage(db.Model):
+    __tablename__ = 'storeItemImages'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('storeItems.id')), nullable=False)
+    url= db.Column(db.String(300), nullable=False)
+
+    storeItems = db.relationship('storeItem', cascade = "all, delete")
+
+   
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'itemId': self.item_id,
+            'url':self.url
+        }
