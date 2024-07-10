8f54f87b8d16a3978ddcd9ac880dc8b619d888b8
Drew Fout <drewmf1566@gmail.com>
2024-07-10 15:40:54 -0500

    orderedItem model

---
+++ app/models/Store/orderedItem.py
@@ -0,0 +1,25 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class orderedItem(db.Model):
+    __tablename__ = 'orderedItems'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('storeOrders.id')), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('storeItems.id')), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    
+    storeOrders = db.relationship('storeOrder', cascade='all, delete')
+    # tags = db.relationship('Topic', cascade= "all, delete")
+    # saves = db.relationship('Save', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'orderId':self.order_id,
+            'itemId': self.item_id,
+            'quantity':self.quantity
+        }
