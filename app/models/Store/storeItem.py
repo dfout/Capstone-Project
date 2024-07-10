3e481ecb576e4c811b7b09032cab1cc73c30c222
Drew Fout <drewmf1566@gmail.com>
2024-07-10 15:25:18 -0500

    storeItem model

---
+++ app/models/Store/storeItem.py
@@ -0,0 +1,33 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class storeItem(db.Model):
+    __tablename__ = 'storeItems'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    avg_rating = db.Column(db.Float, nullable = True)
+    stock = db.Collumn(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
+
+    cartItems = db.relationship('cartItem', cascade = "all, delete")
+    itemCategories = db.relationship('itemCategory', cascade= "all, delete")
+    orderedItems = db.relationship('orderedItem', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'description':self.description,
+            'price': self.price,
+            'avgRating':self.avg_rating,
+            'stock':self.stock,
+            'createdAt':self.created_at,
+            'updatedAt':self.updated_at
+        }
