2f9eeba8a28337de3d2ba5a3bde9480af6438ddf
Drew Fout <drewmf1566@gmail.com>
2024-07-10 16:06:38 -0500

    artist model

---
+++ app/models/Museum/artist.py
@@ -0,0 +1,24 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class artist(db.Model):
+    __tablename__ = 'artists'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable = True)
+    about = db.Column(db.String(1000), nullable = False)
+    born = db.Column(db.DateTime(timezone=True), nullable=True)
+    died = db.Column(db.DateTime(timezone=True), nullable=True)
+
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name': self.name,
+            'about':self.about,
+            'born': self.born,
+            'died': self.died,
+        }
