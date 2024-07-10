b2e3b6abb7c922ebc43bb90ca3cc560955f9e1c3
Drew Fout <drewmf1566@gmail.com>
2024-07-10 15:17:09 -0500

    Review model

---
+++ app/models/Store/review.py
@@ -0,0 +1,31 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.String(200), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('storeItems.id')), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
+
+    storeItems = db.relationship('storeItem', cascade = "all, delete")
+    # tags = db.relationship('Topic', cascade= "all, delete")
+    # saves = db.relationship('Save', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'review':self.review,
+            'rating':self.rating,
+            'itemId': self.item_id,
+            'ownerId':self.user_id,
+            'createdAt':self.created_at,
+            'updatedAt':self.updatedAt
+        }
