a1c056ae83d425884486d10392d77311f9a73a22
Drew Fout <drewmf1566@gmail.com>
2024-07-10 15:37:25 -0500

    storeOrder model

---
+++ app/models/Store/storeOrder.py
@@ -0,0 +1,28 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class storeOrder(db.Model):
+    __tablename__ = 'storeOrders'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    purchaser_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    status= db.Column(db.String(50), nullable=True)
+    purchased_on = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
+
+    users = db.relationship('User', cascade = "all, delete")
+    # tags = db.relationship('Topic', cascade= "all, delete")
+    # saves = db.relationship('Save', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'purchaserId':self.purchaser_id,
+            'status':self.status,
+            'itemId': self.item_id,
+            'purchasedOn':self.purchased_on,
+            'updatedAt':self.updated_at
+        }
