8312d46ea314c3d7a48792061cef0346de7aab6e
Drew Fout <drewmf1566@gmail.com>
2024-07-10 16:12:07 -0500

    exposition model

---
+++ app/models/Museum/exposition.py
@@ -0,0 +1,29 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class exposition(db.Model):
+    __tablename__ = 'expositions'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(1500), nullable=False)
+    gallery_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('galleries.id')), nullable=False)
+    showing_start_date = db.Column(db.DateTime(timezone=True), nullable = True)
+    showing_end_date = db.Column(db.DateTime(timezone=True), nullable = True)
+
+    galleries = db.relationship('galery', cascade = "all, delete")
+
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'description':self.description,
+            'igalleryId': self.gallery_id,
+            'ownerId':self.user_id,
+            'showingStartDate':self.showing_start_date,
+            'showingEndDate':self.showing_end_date
+        }
