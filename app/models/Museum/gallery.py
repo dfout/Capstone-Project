c79d861dd835eb59b92107f61e947444f5d1003d
Drew Fout <drewmf1566@gmail.com>
2024-07-10 16:07:57 -0500

    gallery model

---
+++ app/models/Museum/gallery.py
@@ -0,0 +1,20 @@
+from ..db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class gallery(db.Model):
+    __tablename__ = 'galleries'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable = False)
+    floor = db.Column(db.String(30), nullable = False)
+
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name': self.name,
+            'floor':self.floor,
+        }
