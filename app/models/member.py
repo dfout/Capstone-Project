51ffa6d01075995307b360bc826b30b1198323ba
Drew Fout <drewmf1566@gmail.com>
2024-07-10 16:30:33 -0500

    member and membershipType models

---
+++ app/models/member.py
@@ -0,0 +1,34 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class member(db.Model):
+    __tablename__ = 'members'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    membership_type_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('membershipTypes.id')), nullable=False)
+    curr_card_holders = db.Column(db.Integer, nullable=False)
+    last_charged = db.Column(db.Datetime(timezone=True), nullable=False)
+    next_payment = db.Column(db.Datetime(timezone=True), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), onupdate=db.func.now())
+
+    users = db.relationship('user', cascade = "all, delete")
+    membershipTypes = db.relationship('membershipType', cascade='all, delete')
+
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'userId':self.user_id,
+            'membershipTypeId': self.membership_type_id,
+            'currCardHolders': self.curr_card_holders,
+            'lastCharged': self.last_charged, 
+            'nextPayment': self.next_payment,
+            'createdAt':self.created_at,
+            'updatedAt':self.updated_at
+        }
---
+++ app/models/membershipType.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class membershipType(db.Model):
+    __tablename__ = 'membershipTypes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    billing_cycle = db.Column(db.String(30), nullable=False)
+    price_per_cycle = db.Column(db.Float, nullable=False)
+    cardholders = db.Column(db.Integer, nullable=False)
+
+
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'description': self.description,
+            'billingCycle': self.billing_cycle,
+            'pricePerCycle': self.price_per_cycle, 
+            'cardholders': self.cardholders,
+        }
