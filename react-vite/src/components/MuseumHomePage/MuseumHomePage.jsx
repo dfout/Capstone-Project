67f222c2407a10a8b1ea7619ab1ea01228567940
Drew Fout <drewmf1566@gmail.com>
2024-07-16 23:57:46 -0500

    styling exhibitions on home

--- react-vite/src/components/MuseumHomePage/MuseumHomePage.jsx
+++ react-vite/src/components/MuseumHomePage/MuseumHomePage.jsx
@@ -13,23 +13,68 @@ function MuseumHomePage(){
         dispatch(getExhibitionsThunk())
     }, [dispatch])
 
-
+    // const expositionsArr = Object.values(expositions)
+    //2,
     return(
         <>
         <div className='home-exhibitions-container'>
-            {Object.values(expositions).map((exposition)=>(
-                <div key={exposition.id} className='exposition-tile'>
-                    <NavLink className='exposition-link' to={`/exhibitions/${exposition.id}`}>
-                    <div className='exposition-intro-container'>
-                    <h3 className="archivo-black-regular">{exposition.name}</h3>  
+            {expositions[3] && (
+                <div className='exposition-tile'>
+                    <NavLink className='exposition-link' to={`/exhibitions/${expositions[3].id}`}>
+                    <div className='exposition-intro-container-one'>
+                    <h3 className="archivo-black-regular">{expositions[3].name}</h3>  
                     </div>
+            
                     
                     <div className='exposition-image-container'>
-                            <img id='preview-image' src={exposition.Images[0].url}/>
+                            <img id='preview-image' src={expositions[3].Images[0].url}/>
                         </div>    
                     </NavLink>
                 </div>
-                ))}
+            )}
+             {expositions[7] && (
+                <div className='exposition-tile'>
+                    <NavLink className='exposition-link' to={`/exhibitions/${expositions[7].id}`}>
+                    <div className='exposition-image-container'>
+                            <img id='preview-image' src={expositions[7].Images[0].url}/>
+                        </div>    
+                        <div className='exposition-intro-container-two'>
+                    <h3 className="archivo-black-regular">{expositions[7].name}</h3>  
+                    </div>
+                    </NavLink>
+                </div>
+            )}
+            {expositions[9] && (
+                <div className='exposition-tile'>
+                    <NavLink className='exposition-link' to={`/exhibitions/${expositions[9].id}`}>
+                    <div className='exposition-intro-container-three'>
+                    <h3 className="archivo-black-regular">{expositions[9].name}</h3>  
+                    </div>
+            
+                    
+                    <div className='exposition-image-container'>
+                            <img id='preview-image' src={expositions[9].Images[0].url}/>
+                        </div>    
+                    </NavLink>
+                </div>
+            )}
+            {/* {expositions[6] && (
+                <div className='exposition-tile'>
+                    <NavLink className='exposition-link' to={`/exhibitions/${expositions[6].id}`}>
+                    <div className='exposition-intro-container'>
+                    <h3 className="archivo-black-regular">{expositions[6].name}</h3>  
+                    </div>
+            
+                    
+                    <div className='exposition-image-container'>
+                            <img id='preview-image' src={expositions[6].Images[0].url}/>
+                        </div>    
+                    </NavLink>
+                </div>
+            )} */}
+
+
+
         </div>
         </>
     )
