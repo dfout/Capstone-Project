0add63fe375300734a4901f7ea7c5348ed9897d2
Drew Fout <drewmf1566@gmail.com>
2024-08-02 11:01:15 -0500

    change model

--- app/models/Store/storeItem.py
+++ app/models/Store/storeItem.py
@@ -10,7 +10,7 @@ class StoreItem(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(100000), nullable=False)
     price = db.Column(db.Float, nullable=False)
     avg_rating = db.Column(db.Float, nullable = True)
     stock = db.Column(db.Integer, nullable=False)
